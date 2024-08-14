5ba311d935e04ed7eeb8143445afad266536fb76
virgo <virgostevanus27@gmail.com>
2024-08-14 14:05:42 +0700

    add laravel permission spatie package and fix sidebar active class

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -2,14 +2,14 @@
 <aside id="sidebar" class="sidebar">
     <ul class="sidebar-nav" id="sidebar-nav">
         <li class="nav-item">
-            <a class="nav-link" href="{{ route('dashboard') }}">
+            <a class="nav-link {{ request()->routeIs('dashboard') ? '' : 'collapsed' }}" href="{{ route('dashboard') }}">
                 <i class="bi bi-grid"></i>
                 <span>Dashboard</span>
             </a>
         </li>
 
         <li class="nav-item">
-            <a class="nav-link" href="{{ route('department') }}">
+            <a class="nav-link {{ request()->routeIs('department') ? '' : 'collapsed' }}" href="{{ route('department') }}">
                 <i class="bi bi-building"></i>
                 <span>Department</span>
             </a>
